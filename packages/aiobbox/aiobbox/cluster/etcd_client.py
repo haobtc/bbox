e934642b83ca66e0719fc9488aa85ea34fdaa215
Zeng Ke <superisaac.ke@gmail.com>
2017-06-07 21:05:16 +0800

    bbox lock

--- packages/aiobbox/aiobbox/cluster/etcd_client.py
+++ packages/aiobbox/aiobbox/cluster/etcd_client.py
@@ -1,4 +1,5 @@
 import logging
+import uuid
 import re
 import random
 import asyncio
@@ -57,11 +58,9 @@ class EtcdClient:
             logging.warn('http client error', exc_info=True)
             self.client_failed = True
             raise ETCDError
-        except etcd.EtcdKeyNotFound:
-            raise
         except (etcd.EtcdException, etcd.EtcdConnectionFailed):
-            import traceback
-            traceback.print_exc()
+            #import traceback
+            #traceback.print_exc()
             logging.warn('connection failed')
             self.client_failed = True
             raise ETCDError
@@ -110,4 +109,123 @@ class EtcdClient:
                 logging.warn('etcd error, sleep for a while')
                 await asyncio.sleep(1)
             await changed(None)
-            
+
+    def acquire_lock(self, name):
+        #cfg = get_ticket()
+        #lock_name = name
+        #return Lock(self.client, lock_name)
+        return SimpleLock(self, self.path('_lock/{}'.format(name)))
+
+class SimpleLock:
+    lock_keys = {}
+
+    @classmethod
+    async def close_all_keys(cls, client):
+        for key, u in cls.lock_keys.items():
+            await client.delete(key)
+        cls.lock_keys = {}
+                             
+    async def __aenter__(self):
+        return await self.acquire()
+
+    async def __aexit__(self, exc_type, exc, tb):
+        return await self.release()
+
+    def __init__(self, cc, path):
+        self.client = cc
+        self.path = path
+        self.uuid = uuid.uuid4().hex
+        self.key = None
+        self.cont = True
+        self._acquired = False
+
+    @property
+    def is_acquired(self):
+        return self._acquired
+
+    async def acquire(self):
+        if self.client.client_failed:
+            raise ETCDError
+        r = await self.client.write(self.path, self.uuid, ttl=5, append=True)
+        self.key = r.key
+        self.lock_keys[r.key] = self.uuid
+        asyncio.ensure_future(self.keep_key())        
+        await self.wait_key()
+        return self
+
+    async def release(self):
+        if not self.cont and self.client.client_failed:
+            return
+        if self.key:
+            await self.client.delete(self.key)
+            self.lock_keys.pop(self.key, None)
+            self.key = None
+        else:
+            r = await self.client.read(self.path,
+                                       recursive=True)
+            for n in self.walk(r):
+                if n.value == self.uuid:
+                    await self.client.delete(n.key)
+                    
+        self._acquired = False
+        self.cont = False
+
+    async def check_acquired(self):
+        if self.client.client_failed:
+            self.cont = False
+            return False
+        try:
+            r = await self.client.read(self.path,
+                                       sorted=True,
+                                       recursive=True)
+        except ETCDError:
+            self.cont = False
+            return False
+        waiters = []
+        for n in self.client.walk(r):
+            if self.path == n.key:
+                continue
+            rest_key = n.key[len(self.path):]
+            if re.match(r'/?(?P<name>[^/]+)$', rest_key):
+                waiters.append(n.key)
+        if waiters:
+            # if self.key is the first element
+            # then the lock is acquired
+            if waiters[0] == self.key:
+                self._acquired = True
+                return True
+        return False
+        
+    async def wait_key(self):
+        while self.cont and not (await self.check_acquired()):
+            try:
+                chg = await asyncio.wait_for(
+                    self.client.read(self.path,
+                                     wait=True,
+                                     recursive=True),
+                    timeout=20)
+            except ETCDError:
+                self.cont = False
+                continue
+            except asyncio.TimeoutError:
+                continue
+
+            if (chg.action not in ('delete', 'expire')
+                or chg.key > self.key):
+                # not interest
+                continue
+
+            #await asyncio.sleep(0.1)
+
+    async def keep_key(self):
+        while self.cont and self.key:
+            try:
+                await self.client.refresh(self.key, ttl=5)
+            except etcd.EtcdKeyNotFound:
+                pass
+            except ETCDError:
+                await self.release()
+                #loop = asyncio.get_event_loop()
+                #loop.stop()
+                break
+            await asyncio.sleep(1)
---
+++ packages/aiobbox/aiobbox/tools/watchlock.py
@@ -0,0 +1,53 @@
+import os, sys
+import re
+import json
+import asyncio
+import argparse
+import aiobbox.client as bbox_client
+from aiobbox.cluster import get_cluster, get_sharedconfig, SimpleLock
+from aiobbox.exceptions import ETCDError
+from aiobbox.utils import guess_json, json_pp
+
+parser = argparse.ArgumentParser(
+    prog='bbox lock',
+    description='acquire a lock and execute')
+
+parser.add_argument(
+    'entry',
+    type=str,
+    help='lock entry')
+
+async def main():
+    args, rest_args = parser.parse_known_args()
+    try:
+        await get_cluster().start()
+    except ETCDError:
+        return
+    
+    c = get_cluster()
+    try:
+        async with c.acquire_lock(args.entry) as lock:
+            if lock.is_acquired and rest_args:
+                proc = await asyncio.create_subprocess_shell(
+                    ' '.join(rest_args))
+                await proc.communicate()
+            else:
+                await asyncio.sleep(0.1)
+                
+    finally:
+        pass
+    return True
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    normal = True    
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        normal = False
+    #if not normal:
+    if True:
+        c = get_cluster()
+        loop.run_until_complete(SimpleLock.close_all_keys(c))
+        c.cont = False
+        c.close()
