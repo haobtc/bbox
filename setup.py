cc3d239f885e467608ebaf7a6cdfc15052a2685c
Zeng Ke <superisaac.ke@gmail.com>
2017-07-13 18:33:50 +0800

    install using find_packages

--- setup.py
+++ setup.py
@@ -1,25 +1,12 @@
 from distutils.core import setup
-
+from setuptools import find_packages
 
 setup(name='aiobbox',
       version='0.0.1',
       description='multi-lang, highly available rpc framework',
       author='Zeng Ke',
       author_email='zk@bixin.com',
-      packages=[
-          'aiobbox',
-          'aiobbox.tools',
-          'aiobbox.cluster',
-          'aiobbox.services',
-          'aiobbox.contrib.consumer',
-      ],
-      package_dir={
-          'aiobbox': 'aiobbox',
-          'aiobbox.tools': 'aiobbox/tools',
-          'aiobbox.services': 'aiobbox/services',
-          'aiobbox.cluster': 'aiobbox/cluster',
-          'aiobbox.contrib.consumer': 'aiobbox/contrib/consumer'
-      },
+      packages=find_packages(),
       scripts=['bin/bbox'],
       install_requires=[
           'aiohttp',
