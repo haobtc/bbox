bf2133d2c2e038b5a9a2648d34ed66c51287807e
Zeng Ke <superisaac.ke@gmail.com>
2018-10-14 10:40:02 +0800

    v0.2.5fix; removed the deprecated executable bin/bbox

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='aiobbox',
-      version='0.2.5',
+      version='0.2.6',
       description='multi-lang, highly available rpc framework',
       author='Zeng Ke',
       author_email='zk@bixin.com',
       packages=find_packages(),
-      scripts=['bin/bbox', 'bin/bbox.py', 'bin/bbox-gencert'],
+      scripts=['bin/bbox.py', 'bin/bbox-gencert'],
       install_requires=[
           'aiohttp',
           'aiochannel',
