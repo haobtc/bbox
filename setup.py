8ea77c95b81a4385252879ba81b93c94079b1b9f
Zeng Ke <superisaac.ke@gmail.com>
2018-12-13 14:02:34 +0800

    v0.4.3, added classifiers and python_requires to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='aiobbox',
-      version='0.4.2',
+      version='0.4.3',
       description='multi-lang, highly available rpc framework',
       author='Zeng Ke',
       author_email='zk@bixin.com',
       packages=find_packages(),
       scripts=['bin/bbox.py', 'bin/bbox-gencert'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: MIT',
+          'Programming Language :: Python :: 3.6',
+          'Operating System :: POSIX',
+          'Topic :: Micro-Services',
+      ],
       install_requires=[
           'aiohttp',
           'aiochannel',
@@ -15,5 +24,6 @@ setup(name='aiobbox',
           'aio_etcd',
           'netifaces',
           'aioredis'
-      ]
+      ],
+      python_requires='>=3.6',
 )
