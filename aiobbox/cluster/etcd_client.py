3d96779a778b791a411b03170eb33190ad068cb5
Zeng Ke <superisaac.ke@gmail.com>
2019-06-26 22:46:33 +0800

    Revert "v0.4.12more, raise ETCDError on EtcdException"
    This reverts commit ea83f3335de20b62be33d78358b228d1538f9cf5.

--- aiobbox/cluster/etcd_client.py
+++ aiobbox/cluster/etcd_client.py
@@ -86,7 +86,7 @@ class EtcdClient:
         except etcd.EtcdException:
             logger.warn('etcd exception', exc_info=True)
             self.client_failed = True
-            raise ETCDError
+            raise
 
     async def write(self, *args, **kw):
         return await self._wrap_etcd(self.client.write,
